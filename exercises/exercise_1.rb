e67fd093ca396ba2c29811bab15dfb29b31fa221
remiborris <remiborris@gmail.com>
2024-10-18 20:38:45 -0300

    completed exercises 1-4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,40 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+=begin
+
+- Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+- Richmond (annual_revenue of 1260000 carries women's apparel only)
+- Gastown (annual_revenue of 190000 carries men's apparel only)
+
+=end
+
 # Your code goes below here ...
+
+burnaby = Store.new do |s|
+  s.name = "burnaby"
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+richmond = Store.new do |s|
+  s.name = "richmond"
+  s.annual_revenue = 1260000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+gastown = Store.new do |s|
+  s.name = "gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+burnaby.save
+richmond.save
+gastown.save
+
+stores = Store.all
+puts "Total number of stores: #{stores.count}"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,39 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.new do |s|
+  s.name = "Surrey"
+  s.annual_revenue = 224000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+surrey.save
+
+whistler = Store.new do |s|
+  s.name = "Whistler"
+  s.annual_revenue = 1900000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+whistler.save
+
+yaletown = Store.new do |s|
+  s.name = "Yaletown"
+  s.annual_revenue = 430000
+  s.mens_apparel =  true
+  s.womens_apparel = true
+end
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |s|
+  puts "Name: #{s.name}, Annual Revenue: #{s.annual_revenue}"
+end
+
+@poor_womens_stores = Store.where(womens_apparel: true, annual_revenue: ...1000000)
+
+@poor_womens_stores.each do |s|
+  puts "Name: #{s.name}, Annual Revenue: #{s.annual_revenue}"
+end
